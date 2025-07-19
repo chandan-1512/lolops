b461b357a669415705a93c692c3d2b955d484b17
chandan-1512 <chandan15122003kumar@gmail.com>
2025-07-19 22:44:48 +0530

    added CSS, images, and favicon

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
-from flask import Flask, send_file, jsonify
+from flask import Flask, render_template, jsonify
 import pyjokes
 
 app = Flask(__name__)
 
 @app.route('/')
-def serve_html():
-    return send_file("index.html")
-@app.route('/index.css')
-def serve_css():
-    return send_file("index.css")
-
-@app.route('/emobg.webp')
-def serve_container_bg():
-    return send_file("emobg.webp")
-@app.route('/5335982.jpg')
-def serve_body_bg():
-    return send_file("5335982.jpg")
+def home():
+    return render_template("index.html")
 
 @app.route('/get-joke')
 def get_joke():
@@ -23,4 +13,5 @@ def get_joke():
     return jsonify(joke=joke)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=81)
+
